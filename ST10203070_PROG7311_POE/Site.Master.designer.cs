b9fb63c8cddeb9eef141fc5c29dd0282c65b1f5a
MaxGWalsh <st10203070@vcconnect.edu.za>
2024-05-14 11:49:17 +0200

    Added to nav bar

--- ST10203070_PROG7311_POE/Site.Master.designer.cs
+++ ST10203070_PROG7311_POE/Site.Master.designer.cs
@@ -42,6 +42,15 @@ namespace ST10203070_PROG7311_POE
         protected global::System.Web.UI.HtmlControls.HtmlAnchor ViewProductsLink;
 
         /// <summary>
+        /// FarmerReportLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor FarmerReportLink;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
